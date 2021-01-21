3a3cd2a2da4950cca6a9ad9f02d876703b6a31d2
Itamar Joire <30444253+ItamarJoire@users.noreply.github.com>
2021-01-21 16:23:10 -0300

    dando regras aos forms

--- public/script/app.js
+++ public/script/app.js
@@ -5,48 +5,78 @@ var altura = document.getElementById("altura");
 var nivelAtividade = document.getElementById("atividade");
 
 function calc(){
+
+    checkInputs();
+
     var formulaMasculina = ((10 * pesoAtual.value) + (6.25 * altura.value) - (5 * idade.value) + 5); 
     var formulaFeminina = ((10 * pesoAtual.value) + (6.25 * altura.value) - (5 * idade.value) - 161); 
 
     if(gen.checked){
-       let tmb = formulaMasculina;
-       console.log("Seu tmb é: " + tmb);
+       let TaxaMetabolicaBasal = formulaMasculina;
+       console.log("Seu Taxa metabólica basal é: " + Math.trunc(TaxaMetabolicaBasal));
+       
+       addNivelAtividade(TaxaMetabolicaBasal);
 
-        switch(nivelAtividade.value){
-            case 'sedentario':
-                console.log("Seu TMB sem atividade física: " + Math.trunc(tmb * 1.375));
-                break;
-            case 'poucoativo':
-                console.log("Seu TMB sendo pouco ativo: " + Math.trunc(tmb * 1.55));
-                break;
-            case 'ativo':
-                console.log("Seu TMB sendo Ativo: " + Math.trunc(tmb * 1.725));
-                break;
-            case 'muitoativo':
-                console.log("Seu TMB sendo Muito ativo - Atleta: " + Math.trunc(tmb * 1.9));
-                break;
-            default:
-                console.log("Não selecionou nenhum nível de atividade!");
-        }
     }else{
-        let tmb = formulaFeminina;
-        console.log("Sem tmb fem é: " + tmb);
+        let TaxaMetabolicaBasal = formulaFeminina;
+        console.log("Sua Taxa Metabolica Basal é: " + Math.trunc(TaxaMetabolicaBasal));
 
-        switch(nivelAtividade.value){
-            case 'sedentario':
-                console.log("Seu TMB sem atividade física: " + Math.trunc(tmb * 1.375));
-                break;
-            case 'poucoativo':
-                console.log("Seu TMB sendo pouco ativo: " + Math.trunc(tmb * 1.55));
-                break;
-            case 'ativo':
-                console.log("Seu TMB sendo Ativo: " + Math.trunc(tmb * 1.725));
-                break;
-            case 'muitoativo':
-                console.log("Seu TMB sendo Muito ativo - Atleta: " + Math.trunc(tmb * 1.9));
-                break;
-            default:
-                console.log("Não selecionou nenhum nível de atividade!");
-        }
+        addNivelAtividade(TaxaMetabolicaBasal);
     }
 }
+
+function checkInputs(){
+    if(idade.value === ''){
+        inputVazio(idade);
+    }else{
+        inputPreenchido(idade)
+    }
+
+    if(pesoAtual.value === ''){
+        inputVazio(pesoAtual);
+    }else{
+        inputPreenchido(pesoAtual)
+    }
+
+    if(altura.value === ''){
+        inputVazio(altura);
+    }else{
+        inputPreenchido(altura)
+    }
+}
+
+
+
+function inputVazio(input){
+    const formControl = input.parentElement;
+    formControl.className = 'flex error';  
+}
+
+function inputPreenchido(input){
+    const formControl = input.parentElement;
+    formControl.className = 'flex sucess';  
+}
+
+
+
+
+
+function addNivelAtividade(tmb){
+    switch(nivelAtividade.value){
+        case 'sedentario':
+            console.log("Sua Taxa metabólica basal sem atividade física: " + Math.trunc(tmb * 1.375));
+            break;
+        case 'poucoativo':
+            console.log("Sua Taxa metabólica basal sendo pouco ativo: " + Math.trunc(tmb * 1.55));
+            break;
+        case 'ativo':
+            console.log("Sua Taxa metabólica basal sendo ativo: " + Math.trunc(tmb * 1.725));
+            break;
+        case 'muitoativo':
+            console.log("Sua Taxa metabólica basal sendo muito ativo - Atleta: " + Math.trunc(tmb * 1.9));
+            break;
+        default:
+            console.log("Não selecionou nenhum nível de atividade!");
+    }
+}
+
