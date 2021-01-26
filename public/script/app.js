39935565d2c1866f8649bd5b194379647606ea1c
Itamar Joire <30444253+ItamarJoire@users.noreply.github.com>
2021-01-26 00:33:27 -0300

    cards

--- public/script/app.js
+++ public/script/app.js
@@ -4,7 +4,12 @@ var pesoAtual = document.getElementById("peso-atual");
 var altura = document.getElementById("altura");
 var nivelAtividade = document.getElementById("atividade");
 
-var resultado = document.getElementById("view");
+var cards = document.querySelector(".hero-card");
+cards.style.display = "none";
+
+var cardOne = document.querySelector(".fundo-1");
+var cardTwo = document.querySelector(".fundo-2");
+var cardThree = document.querySelector(".fundo-3");
 
 function calc(){
     checkInputs(); 
@@ -25,10 +30,15 @@ function calcPermitido(){
 
     if(gen.checked){
         let TaxaMetabolicaBasal = formulaMasculina;
-        resultado.innerHTML = `<tr><td> Sua TMB é:</td><td>${Math.trunc(TaxaMetabolicaBasal)}</td>`
+        cards.style.display = "block";
+        cardOne.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia</strong></p>`
+
+        cardTwo.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia</strong></p>`
+
+        cardThree.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia</strong></p>`
 
         addNivelAtividade(TaxaMetabolicaBasal);
-
+        
     }else{
         let TaxaMetabolicaBasal = formulaFeminina;
         resultado.innerHTML = `<tr><td> Sua TMB é:</td><td>${Math.trunc(TaxaMetabolicaBasal)}</td>`
@@ -45,9 +55,35 @@ function inputVazio(){
 function addNivelAtividade(tmb){
     switch(nivelAtividade.value){
         case 'sedentario':
-            resultado.innerHTML += `<tr><td> Sua Taxa metabólica basal sem atividade física é: </td><td>${Math.trunc(tmb * 1.375)}</td>`
-            resultado.className = "view orange";
-            break;
+            let proteina = Math.trunc(2.3 * pesoAtual.value);
+            let gordura = Math.trunc(0.8 * pesoAtual.value);
+            let carboidrato = Math.trunc((tmb - (proteina * 4) - (gordura * 9)) / 4);
+            cardOne.innerHTML += `<p class="consumo"> Consumir <strong class="name-white">${Math.trunc(tmb * 1.2)} calorias por dia</strong> para manter o peso com os seguintes macronutrientes:</p>
+            <ul class="macros"> 
+                <li><strong class="name-white">${proteina}g</strong> de proteínas</li>
+                <li><strong class="name-white">${gordura}g</strong> de gorduras</li>
+                <li><strong class="name-white">${carboidrato}g</strong> de carboidratos</li>
+            </ul>
+            `
+            cardTwo.innerHTML += `<p class="consumo"> Consumir <strong class="name-white">${Math.trunc(tmb * 1.2)} calorias por dia</strong> para perder peso com os seguintes macronutrientes:</p>
+            <ul class="macros"> 
+                <li><strong class="name-white">${Math.trunc(2 * pesoAtual.value)}g</strong> de proteínas</li>
+                <li><strong class="name-white">${Math.trunc(0.8 * pesoAtual.value)}g</strong> de gorduras</li>
+                <li><strong class="name-white">${Math.trunc(3 * pesoAtual.value)}g</strong> de carboidratos</li>
+            </ul>
+            `
+
+            cardThree.innerHTML += `<p class="consumo"> Consumir <strong class="name-white">${Math.trunc(tmb * 1.2)} calorias por dia</strong> para perder peso com os seguintes macronutrientes:</p>
+            <ul class="macros"> 
+                <li><strong class="name-white">${Math.trunc(2 * pesoAtual.value)}g</strong> de proteínas</li>
+                <li><strong class="name-white">${Math.trunc(0.8 * pesoAtual.value)}g</strong> de gorduras</li>
+                <li><strong class="name-white">${Math.trunc(3 * pesoAtual.value)}g</strong> de carboidratos</li>
+            </ul>
+            `
+
+
+
+
         case 'poucoativo':
             resultado.innerHTML += `<tr><td> Sua Taxa metabólica basal sendo POUCO ATIVO é: </td><td>${Math.trunc(tmb * 1.55)}</td>`
             break;
