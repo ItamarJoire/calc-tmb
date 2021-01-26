a114a95ab04382ebefd7fb3bd9fe5642d69c2046
Itamar Joire <30444253+ItamarJoire@users.noreply.github.com>
2021-01-26 18:51:51 -0300

    calculadora concluída

--- public/script/app.js
+++ public/script/app.js
@@ -8,8 +8,6 @@ var cards = document.querySelector(".hero-card");
 cards.style.display = "none";
 
 var cardOne = document.querySelector(".fundo-1");
-var cardTwo = document.querySelector(".fundo-2");
-var cardThree = document.querySelector(".fundo-3");
 
 function calc(){
     checkInputs(); 
@@ -31,17 +29,14 @@ function calcPermitido(){
     if(gen.checked){
         let TaxaMetabolicaBasal = formulaMasculina;
         cards.style.display = "block";
-        cardOne.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia</strong></p>`
-
-        cardTwo.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia</strong></p>`
-
-        cardThree.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia</strong></p>`
+        cardOne.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia.</strong></p>`
 
         addNivelAtividade(TaxaMetabolicaBasal);
         
     }else{
         let TaxaMetabolicaBasal = formulaFeminina;
-        resultado.innerHTML = `<tr><td> Sua TMB é:</td><td>${Math.trunc(TaxaMetabolicaBasal)}</td>`
+        cards.style.display = "block";
+        cardOne.innerHTML = `<p class="tmb">Sua taxa metabólica basal é de <strong class="name-white">${Math.trunc(TaxaMetabolicaBasal)} calorias por dia.</strong></p>`
 
         addNivelAtividade(TaxaMetabolicaBasal);
     }
@@ -52,50 +47,72 @@ function inputVazio(){
 }
 
 
+
 function addNivelAtividade(tmb){
     switch(nivelAtividade.value){
         case 'sedentario':
-            let proteina = Math.trunc(2.3 * pesoAtual.value);
-            let gordura = Math.trunc(0.8 * pesoAtual.value);
-            let carboidrato = Math.trunc((tmb - (proteina * 4) - (gordura * 9)) / 4);
-            cardOne.innerHTML += `<p class="consumo"> Consumir <strong class="name-white">${Math.trunc(tmb * 1.2)} calorias por dia</strong> para manter o peso com os seguintes macronutrientes:</p>
+            var caloriasSedentario = tmb * 1.2;
+            var proteina = Math.trunc(2 * pesoAtual.value);
+            var gordura = Math.trunc(0.6 * pesoAtual.value);
+            var carboidrato = Math.trunc((caloriasSedentario - (proteina * 4) - (gordura * 9)) / 4);
+            
+            cardOne.innerHTML += `<p class="consumo"> Nível de atividade:<strong class="name-white"> Sedentário.</strong><br> Manter o peso: <strong class="name-white">${Math.trunc(caloriasSedentario)} calorias por dia.</strong><br> Perda de peso: <strong class="name-white">${Math.trunc(caloriasSedentario - 500)} calorias por dia.</p>
             <ul class="macros"> 
-                <li><strong class="name-white">${proteina}g</strong> de proteínas</li>
-                <li><strong class="name-white">${gordura}g</strong> de gorduras</li>
-                <li><strong class="name-white">${carboidrato}g</strong> de carboidratos</li>
-            </ul>
-            `
-            cardTwo.innerHTML += `<p class="consumo"> Consumir <strong class="name-white">${Math.trunc(tmb * 1.2)} calorias por dia</strong> para perder peso com os seguintes macronutrientes:</p>
+                <li>Macronutrientes</li>
+                <li><strong class="name-white">${proteina}g</strong> de proteínas.</li>
+                <li><strong class="name-white">${gordura}g</strong> de gorduras.</li>
+                <li><strong class="name-white">${carboidrato}g</strong> de carboidratos.</li>
+            </ul>`
+            break;
+
+            case 'leve':
+                var caloriasLeve = tmb * 1.375;
+                var proteina = Math.trunc(2.1 * pesoAtual.value);
+                var gordura = Math.trunc(0.7 * pesoAtual.value);
+                var carboidrato = Math.trunc((caloriasLeve - (proteina * 4) - (gordura * 9)) / 4);
+
+                cardOne.innerHTML += `<p class="consumo"> Nível de atividade:<strong class="name-white"> Exercício leve.</strong><br> Manter o peso: <strong class="name-white">${Math.trunc(caloriasLeve)} calorias por dia.</strong><br> Perda de peso: <strong class="name-white">${Math.trunc(caloriasLeve - 500)} calorias por dia.</p>
             <ul class="macros"> 
-                <li><strong class="name-white">${Math.trunc(2 * pesoAtual.value)}g</strong> de proteínas</li>
-                <li><strong class="name-white">${Math.trunc(0.8 * pesoAtual.value)}g</strong> de gorduras</li>
-                <li><strong class="name-white">${Math.trunc(3 * pesoAtual.value)}g</strong> de carboidratos</li>
-            </ul>
-            `
+                <li>Macronutrientes</li>
+                <li><strong class="name-white">${proteina}g</strong> de proteínas.</li>
+                <li><strong class="name-white">${gordura}g</strong> de gorduras.</li>
+                <li><strong class="name-white">${carboidrato}g</strong> de carboidratos.</li>
+            </ul>`
+            break;
 
-            cardThree.innerHTML += `<p class="consumo"> Consumir <strong class="name-white">${Math.trunc(tmb * 1.2)} calorias por dia</strong> para perder peso com os seguintes macronutrientes:</p>
+            case 'moderado':
+                var caloriasModerado = tmb * 1.55;
+                var proteina = Math.trunc(2.6 * pesoAtual.value);
+                var gordura = Math.trunc(0.6 * pesoAtual.value);
+                var carboidrato = Math.trunc((caloriasModerado - (proteina * 4) - (gordura * 9)) / 4);
+
+                cardOne.innerHTML += `<p class="consumo"> Nível de atividade:<strong class="name-white"> Exercício moderado.</strong><br> Manter o peso: <strong class="name-white">${Math.trunc(caloriasModerado)} calorias por dia.</strong><br> Perda de peso: <strong class="name-white">${Math.trunc(caloriasModerado - 500)} calorias por dia.</p>
             <ul class="macros"> 
-                <li><strong class="name-white">${Math.trunc(2 * pesoAtual.value)}g</strong> de proteínas</li>
-                <li><strong class="name-white">${Math.trunc(0.8 * pesoAtual.value)}g</strong> de gorduras</li>
-                <li><strong class="name-white">${Math.trunc(3 * pesoAtual.value)}g</strong> de carboidratos</li>
-            </ul>
-            `
-
-
-
-
-        case 'poucoativo':
-            resultado.innerHTML += `<tr><td> Sua Taxa metabólica basal sendo POUCO ATIVO é: </td><td>${Math.trunc(tmb * 1.55)}</td>`
+                <li>Macronutrientes</li>
+                <li><strong class="name-white">${proteina}g</strong> de proteínas.</li>
+                <li><strong class="name-white">${gordura}g</strong> de gorduras.</li>
+                <li><strong class="name-white">${carboidrato}g</strong> de carboidratos.</li>
+            </ul>`
             break;
-        case 'ativo':
-            resultado.innerHTML += `<tr><td> Sua Taxa metabólica basal sendo ATIVO é: </td><td>${Math.trunc(tmb * 1.725)}</td>`
+
+            case 'pesado':
+                var caloriasPesado = tmb * 1.725;
+                var proteina = Math.trunc(3 * pesoAtual.value);
+                var gordura = Math.trunc(0.9 * pesoAtual.value);
+                var carboidrato = Math.trunc((caloriasPesado - (proteina * 4) - (gordura * 9)) / 4);
+
+                cardOne.innerHTML += `<p class="consumo"> Nível de atividade:<strong class="name-white"> Exercício pesado.</strong><br> Manter o peso: <strong class="name-white">${Math.trunc(caloriasPesado)} calorias por dia.</strong><br> Perda de peso: <strong class="name-white">${Math.trunc(caloriasPesado - 500)} calorias por dia.</p>
+            <ul class="macros"> 
+                <li>Macronutrientes</li>
+                <li><strong class="name-white">${proteina}g</strong> de proteínas.</li>
+                <li><strong class="name-white">${gordura}g</strong> de gorduras.</li>
+                <li><strong class="name-white">${carboidrato}g</strong> de carboidratos.</li>
+            </ul>`
             break;
-        case 'muitoativo':
-            resultado.innerHTML += `<tr><td> Sua Taxa metabólica basal sendo MUITO ATIVO é: </td><td>${Math.trunc(tmb * 1.9)}</td>`
-            break;
-        default:
-            console.log("Não selecionou nenhum nível de atividade!");
-    }
+
+            default:
+                console.log("Não selecionou nenhum nível de atividade!");
+        }
 }
 
 
